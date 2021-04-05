06b04e4f7499b892507b3efa090eda2db7e65482
Maddy@Xdev200 <xdev200200@gmail.com>
2021-04-05 22:17:06 +0530

    removed:console.log

--- src/reducers/cart.js
+++ src/reducers/cart.js
@@ -1,31 +1,29 @@
-export const initialState ={
-    cartItem:[],
-    error:''
-  }
-  
-  export const cartReducer = (state,action)=>{
-    switch(action.type){
-      case 'AddToCart':
-        console.log(state.cartItem)
-    
-        return {...state,
-            cartItem:[...state.cartItem,action.payload],
-          error:''
-        }
+export const initialState = {
+  cartItem: [],
+  error: "",
+};
 
-      case 'AddToCartRepeated':
-        return {
-          cartItem:action.payload.product,
-          error:''
-        }
+export const cartReducer = (state, action) => {
+  switch (action.type) {
+    case "AddToCart":
+      return {
+        ...state,
+        cartItem: [...state.cartItem, action.payload],
+        error: "",
+      };
 
-       
-      case 'OnFailure':
-        return{
-          cartItem:[],
-          error:'Unable to Load Cart, Try after sometime'
-        }
-      default:
-        return state
-    }
+    case "AddToCartRepeated":
+      return {
+        cartItem: action.payload.product,
+        error: "",
+      };
+
+    case "OnFailure":
+      return {
+        cartItem: [],
+        error: "Unable to Load Cart, Try after sometime",
+      };
+    default:
+      return state;
   }
+};
