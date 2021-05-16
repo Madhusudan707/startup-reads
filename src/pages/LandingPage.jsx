364243585e0ca3d6407f9d71ffe754b83c53a971
Maddy@Xdev200 <xdev200200@gmail.com>
2021-05-16 11:46:17 +0530

    update: navigation

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -3,12 +3,18 @@ import {Landing} from '../Components'
 import {useNavigate} from 'react-router-dom'
 import {useLogin } from "../contexts";
 import './style.css'
+import { useEffect } from 'react';
 export const LandingPage = () => {
     const navigate = useNavigate();
     const { login} = useLogin();
+    useEffect(()=>{
+      if(login){
+        navigate("/home")
+      }
+    },[])
     return (
         <div className="flex justify-center items-center  w-full  h-screen border landing-page">
-          {login?navigate("/home") :<Landing/>}  
+          <Landing/>
         </div>
     )
 }
