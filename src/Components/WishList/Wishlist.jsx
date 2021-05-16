541b57245453087e9cc8cb929caf5ff18825c1fd
Maddy@Xdev200 <xdev200200@gmail.com>
2021-05-16 11:55:02 +0530

    add: remove feature

--- src/Components/WishList/Wishlist.jsx
+++ src/Components/WishList/Wishlist.jsx
@@ -3,6 +3,7 @@ import { useFetchLibrary } from "../../hooks";
 
 export const WishList = () => {
   const { wishListState } = useFetchLibrary();
+  console.log(wishListState.wishes)
   return (
     <div className="flex flex-col justify-center p-1 flex-wrap mt-72 lg:flex-row lg:p-10 lg:mt-52 lg:w-full">
       {wishListState.wishes.map((book) => {
@@ -12,8 +13,11 @@ export const WishList = () => {
             bookTitle={book.title}
             imageURL={book.image}
             price={book.price}
-            isHide={true}
+            wish={book.wish}
+            isHide={false}
             product={book}
+            isbn={book.isbn}
+            _id={book._id}
           />
         );
       })}
