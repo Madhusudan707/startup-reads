716d4a5cef735f1cc9ee144104100d9b22165075
Maddy@Xdev200 <xdev200200@gmail.com>
2021-05-19 07:26:02 +0530

    add: loader to cart

--- src/Components/Reusable/Counter.jsx
+++ src/Components/Reusable/Counter.jsx
@@ -1,29 +1,30 @@
-import {Button} from './'
-import { useCounterHandler } from "../../hooks"
+import { Button, Loader } from "./";
+import { useCounterHandler } from "../../hooks";
 
-export const Counter = ({ itemCount,pid}) => {
-  const { counterHandler, btnDisable } = useCounterHandler();
+export const Counter = ({ itemCount, pid }) => {
+  const { counterHandler, btnDisable, counterLoading } = useCounterHandler();
 
   return (
-    <div className='flex flex-row items-center justify-around'>
-       <Button
+    <div className="flex flex-row items-center justify-around">
+      <Button
         isDisabled={btnDisable}
         nameClass="bg-blue-400 rounded-full h-8 w-8 text-white font-bold text-2xl flex items-center justify-center"
         text="-"
         btnFunction={() => {
-          counterHandler({itemCount,incDec:"-",pid});
+          counterHandler({ itemCount, incDec: "-", pid });
         }}
       />
-      <span className='border-4 bg-red-500 text-white font-bold   px-2 '>{itemCount}</span>
-
-    
-       <Button
+      <span className="border-4 bg-red-500 text-white font-bold   px-2 ">
+        {itemCount}
+      </span>
+      <Button
         text="+"
         nameClass="bg-blue-400 rounded-full h-8 w-8 text-white font-bold text-2xl flex items-center justify-center"
         btnFunction={() => {
-          counterHandler({itemCount,incDec:"+",pid});
+          counterHandler({ itemCount, incDec: "+", pid });
         }}
       />
+      <Loader toggle={counterLoading ? "opacity-50" : "opacity-0"} />
     </div>
   );
 };
--- src/hooks/useCounterHandler.js
+++ src/hooks/useCounterHandler.js
@@ -10,46 +10,58 @@ export const useCounterHandler = () => {
   const { removeItemFromCart } = useRemoveItemFromCart();
   const { api } = useAPI();
   const { setToastMsg, setToastColor, SUCCESS, DANGER } = useToast();
+  const [counterLoading, setCounterLoading] = useState(false);
 
   const counterHandler = async ({ itemCount, incDec, pid }) => {
+    setCounterLoading(true);
+
+    let response = "";
     const userId = localStorage.getItem("_id");
-    const newArr = cartState.cartItem.map((item) => {
-      if (item._id === pid) {
-        if (incDec === "+") {
-          setToastMsg(`ITEM ADDED TO CART`);
-          setToastColor(SUCCESS);
-          counterDispatch({ type: "INCREMENT" });
-          return { ...item, qty: item.qty + 1 };
-        } else {
-          if (itemCount > 1) {
-            counterDispatch({ type: "DECREMENT" });
-            setBtnDisable(false);
-            setToastMsg(`ITEM REMOVED FROM CART`);
-            setToastColor(DANGER);
-            return { ...item, qty: item.qty - 1 };
-          } else {
-            setToastMsg(`ITEM REMOVED FROM CART`);
-            setToastColor(DANGER);
-            removeItemFromCart(item._id);
-          }
-        }
-      }
-      return item;
-    });
 
     try {
-      await axios.post(
+      response = await axios.post(
         `${api.URL}${api.usersActivity.POST}user/${userId}/cart/update/${incDec}/${pid}`
       );
-    } catch (err) {
-      console.log(`${err}:Unable to update cart`);
-    }
 
-    cartDispatch({
-      type: "ADD_TO_CART_REPEATED",
-      payload: { product: newArr },
-    });
+      if (response.status === 200) {
+        const newArr = cartState.cartItem.map((item) => {
+          if (item._id === pid) {
+            if (incDec === "+") {
+              setToastMsg(`ITEM ADDED TO CART`);
+              setToastColor(SUCCESS);
+              counterDispatch({ type: "INCREMENT" });
+              return { ...item, qty: item.qty + 1 };
+            } else {
+              if (itemCount > 1) {
+                counterDispatch({ type: "DECREMENT" });
+                setBtnDisable(false);
+                setToastMsg(`ITEM REMOVED FROM CART`);
+                setToastColor(DANGER);
+                return { ...item, qty: item.qty - 1 };
+              } else {
+                setToastMsg(`ITEM REMOVED FROM CART`);
+                setToastColor(DANGER);
+                removeItemFromCart(item._id);
+              }
+            }
+          }
+          return item;
+        });
+
+        cartDispatch({
+          type: "ADD_TO_CART_REPEATED",
+          payload: { product: newArr },
+        });
+
+        setCounterLoading(false);
+
+      }
+    } catch (err) {
+      setToastMsg(`${err}:UNABLE TO UPDATE CART`);
+      setToastColor(DANGER);
+      setCounterLoading(false);
+    }
   };
 
-  return { counterHandler, btnDisable };
+  return { counterHandler, btnDisable,counterLoading };
 };
