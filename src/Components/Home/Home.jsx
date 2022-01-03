ebb34616fc0fb6967777e4d63528d8d430f23720
xdev200 <xdev200200@gmail.com>
2022-01-03 23:06:18 +0530

    update: redesign

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,9 +1,11 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { ProductCard } from "../";
 import { Widget, Loader } from "../Reusable";
 import { useFetchLibrary } from "../../hooks";
+import { Link } from "react-router-dom";
 
 export const Home = () => {
+  const [readMore, setReadMore] = useState(false);
   const { refresh, setRefresh, libraryLoading } = useFetchLibrary();
   useEffect(() => {
     (async () => {
@@ -12,10 +14,321 @@ export const Home = () => {
     //Below Line remove the useEffect dependency warning
     //eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+  const readMoreHandler = () => {
+    console.log("hello");
+    setReadMore(!readMore);
+  };
   return (
     <div>
       {/* <Widget /> */}
-      {libraryLoading ? <Loader toggle="opacity-50" /> : <ProductCard />}
+      {libraryLoading ? (
+        <Loader toggle="opacity-50" />
+      ) : (
+        <div className="mt-24 lg:py-20 mx-5 lg:mx-20 mb-20">
+          <div className="flex justify-center w-full px-0 sm:px-10 md:px-16 lg:px-20">
+            <img src="images/hero.svg" alt="hero" className="lg:h-96" />
+          </div>
+
+          <div className="text-2xl font-bold text-center text-blue-400 pt-8 pb-2 lg:text-4xl">
+            Digitally Course
+          </div>
+          <div className="text-sm font-light tracking-wider text-center lg:text-lg">
+            Start Learning Smartly
+          </div>
+          <div className="flex justify-center w-full mt-5">
+            <Link
+              to="/"
+              className="text-white bg-blue-400 text-center rounded-lg px-7 py-3 font-bold hover:bg-blue-500 hover:shadow-lg "
+            >
+              Explore Courses
+            </Link>
+          </div>
+
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 md:gap-10 lg:gap-16 py-20">
+            <div className="border-2 border-blue-400 rounded-lg p-4 flex flex-col items-center">
+              <div>
+                <img src="images/course.png" alt="course" />
+              </div>
+              <div className="font-bold text-xl pt-3 pb-2">Course Name</div>
+              <div
+                className={`${
+                  readMore ? "" : "line-clamp-2"
+                } text-sm  font-light text-gray-700 tracking-wide text-justify`}
+              >
+                lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem
+                ipsum dolor sit ametlorem ipsum dolor sit amet lorem ipsum dolor
+                sit amet lorem ipsum dolor sit amet
+              </div>
+              <div
+                onClick={readMoreHandler}
+                className="self-end text-xs text-blue-400 font-medium py-2"
+              >
+                {readMore ? "Show Less" : "Read More"}
+              </div>
+              <div className="py-2 flex">
+                <div className="line-through font-bold text-blue-400">
+                  ₹ 400/-
+                </div>
+                <div className="font-bold pl-2">₹ 200/-</div>
+              </div>
+
+              <button className="text-white bg-blue-400 text-center rounded-lg w-full mt-2 py-3 font-bold hover:bg-blue-500 hover:shadow-lg ">
+                Buy Now
+              </button>
+            </div>
+            <div className="border-2 border-blue-400 rounded-lg p-4 flex flex-col items-center">
+              <div>
+                <img src="images/course.png" alt="course" />
+              </div>
+              <div className="font-bold text-xl pt-3 pb-2">Course Name</div>
+              <div
+                className={`${
+                  readMore ? "" : "line-clamp-2"
+                } text-sm  font-light text-gray-700 tracking-wide text-justify`}
+              >
+                lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem
+                ipsum dolor sit ametlorem ipsum dolor sit amet lorem ipsum dolor
+                sit amet lorem ipsum dolor sit amet
+              </div>
+              <div
+                onClick={readMoreHandler}
+                className="self-end text-xs text-blue-400 font-medium py-2"
+              >
+                {readMore ? "Show Less" : "Read More"}
+              </div>
+              <div className="py-2 flex">
+                <div className="line-through font-bold text-blue-400">
+                  ₹ 400/-
+                </div>
+                <div className="font-bold pl-2">₹ 200/-</div>
+              </div>
+
+              <button className="text-white bg-blue-400 text-center rounded-lg w-full mt-2 py-3 font-bold hover:bg-blue-500 hover:shadow-lg ">
+                Buy Now
+              </button>
+            </div>
+            <div className="border-2 border-blue-400 rounded-lg p-4 flex flex-col items-center">
+              <div>
+                <img src="images/course.png" alt="course" />
+              </div>
+              <div className="font-bold text-xl pt-3 pb-2">Course Name</div>
+              <div
+                className={`${
+                  readMore ? "" : "line-clamp-2"
+                } text-sm  font-light text-gray-700 tracking-wide text-justify`}
+              >
+                lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem
+                ipsum dolor sit ametlorem ipsum dolor sit amet lorem ipsum dolor
+                sit amet lorem ipsum dolor sit amet
+              </div>
+              <div
+                onClick={readMoreHandler}
+                className="self-end text-xs text-blue-400 font-medium py-2"
+              >
+                {readMore ? "Show Less" : "Read More"}
+              </div>
+              <div className="py-2 flex">
+                <div className="line-through font-bold text-blue-400">
+                  ₹ 400/-
+                </div>
+                <div className="font-bold pl-2">₹ 200/-</div>
+              </div>
+
+              <button className="text-white bg-blue-400 text-center rounded-lg w-full mt-2 py-3 font-bold hover:bg-blue-500 hover:shadow-lg ">
+                Buy Now
+              </button>
+            </div>
+            <div className="border-2 border-blue-400 rounded-lg p-4 flex flex-col items-center">
+              <div>
+                <img src="images/course.png" alt="course" />
+              </div>
+              <div className="font-bold text-xl pt-3 pb-2">Course Name</div>
+              <div
+                className={`${
+                  readMore ? "" : "line-clamp-2"
+                } text-sm  font-light text-gray-700 tracking-wide text-justify`}
+              >
+                lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem
+                ipsum dolor sit ametlorem ipsum dolor sit amet lorem ipsum dolor
+                sit amet lorem ipsum dolor sit amet
+              </div>
+              <div
+                onClick={readMoreHandler}
+                className="self-end text-xs text-blue-400 font-medium py-2"
+              >
+                {readMore ? "Show Less" : "Read More"}
+              </div>
+              <div className="py-2 flex">
+                <div className="line-through font-bold text-blue-400">
+                  ₹ 400/-
+                </div>
+                <div className="font-bold pl-2">₹ 200/-</div>
+              </div>
+
+              <button className="text-white bg-blue-400 text-center rounded-lg w-full mt-2 py-3 font-bold hover:bg-blue-500 hover:shadow-lg ">
+                Buy Now
+              </button>
+            </div>
+            <div className="border-2 border-blue-400 rounded-lg p-4 flex flex-col items-center">
+              <div>
+                <img src="images/course.png" alt="course" />
+              </div>
+              <div className="font-bold text-xl pt-3 pb-2">Course Name</div>
+              <div
+                className={`${
+                  readMore ? "" : "line-clamp-2"
+                } text-sm  font-light text-gray-700 tracking-wide text-justify`}
+              >
+                lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem
+                ipsum dolor sit ametlorem ipsum dolor sit amet lorem ipsum dolor
+                sit amet lorem ipsum dolor sit amet
+              </div>
+              <div
+                onClick={readMoreHandler}
+                className="self-end text-xs text-blue-400 font-medium py-2"
+              >
+                {readMore ? "Show Less" : "Read More"}
+              </div>
+              <div className="py-2 flex">
+                <div className="line-through font-bold text-blue-400">
+                  ₹ 400/-
+                </div>
+                <div className="font-bold pl-2">₹ 200/-</div>
+              </div>
+
+              <button className="text-white bg-blue-400 text-center rounded-lg w-full mt-2 py-3 font-bold hover:bg-blue-500 hover:shadow-lg ">
+                Buy Now
+              </button>
+            </div>
+            <div className="border-2 border-blue-400 rounded-lg p-4 flex flex-col items-center">
+              <div>
+                <img src="images/course.png" alt="course" />
+              </div>
+              <div className="font-bold text-xl pt-3 pb-2">Course Name</div>
+              <div
+                className={`${
+                  readMore ? "" : "line-clamp-2"
+                } text-sm  font-light text-gray-700 tracking-wide text-justify`}
+              >
+                lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem
+                ipsum dolor sit ametlorem ipsum dolor sit amet lorem ipsum dolor
+                sit amet lorem ipsum dolor sit amet
+              </div>
+              <div
+                onClick={readMoreHandler}
+                className="self-end text-xs text-blue-400 font-medium py-2"
+              >
+                {readMore ? "Show Less" : "Read More"}
+              </div>
+              <div className="py-2 flex">
+                <div className="line-through font-bold text-blue-400">
+                  ₹ 400/-
+                </div>
+                <div className="font-bold pl-2">₹ 200/-</div>
+              </div>
+
+              <button className="text-white bg-blue-400 text-center rounded-lg w-full mt-2 py-3 font-bold hover:bg-blue-500 hover:shadow-lg ">
+                Buy Now
+              </button>
+            </div>
+            <div className="border-2 border-blue-400 rounded-lg p-4 flex flex-col items-center">
+              <div>
+                <img src="images/course.png" alt="course" />
+              </div>
+              <div className="font-bold text-xl pt-3 pb-2">Course Name</div>
+              <div
+                className={`${
+                  readMore ? "" : "line-clamp-2"
+                } text-sm  font-light text-gray-700 tracking-wide text-justify`}
+              >
+                lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem
+                ipsum dolor sit ametlorem ipsum dolor sit amet lorem ipsum dolor
+                sit amet lorem ipsum dolor sit amet
+              </div>
+              <div
+                onClick={readMoreHandler}
+                className="self-end text-xs text-blue-400 font-medium py-2"
+              >
+                {readMore ? "Show Less" : "Read More"}
+              </div>
+              <div className="py-2 flex">
+                <div className="line-through font-bold text-blue-400">
+                  ₹ 400/-
+                </div>
+                <div className="font-bold pl-2">₹ 200/-</div>
+              </div>
+
+              <button className="text-white bg-blue-400 text-center rounded-lg w-full mt-2 py-3 font-bold hover:bg-blue-500 hover:shadow-lg ">
+                Buy Now
+              </button>
+            </div>
+            <div className="border-2 border-blue-400 rounded-lg p-4 flex flex-col items-center">
+              <div>
+                <img src="images/course.png" alt="course" />
+              </div>
+              <div className="font-bold text-xl pt-3 pb-2">Course Name</div>
+              <div
+                className={`${
+                  readMore ? "" : "line-clamp-2"
+                } text-sm  font-light text-gray-700 tracking-wide text-justify`}
+              >
+                lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem
+                ipsum dolor sit ametlorem ipsum dolor sit amet lorem ipsum dolor
+                sit amet lorem ipsum dolor sit amet
+              </div>
+              <div
+                onClick={readMoreHandler}
+                className="self-end text-xs text-blue-400 font-medium py-2"
+              >
+                {readMore ? "Show Less" : "Read More"}
+              </div>
+              <div className="py-2 flex">
+                <div className="line-through font-bold text-blue-400">
+                  ₹ 400/-
+                </div>
+                <div className="font-bold pl-2">₹ 200/-</div>
+              </div>
+
+              <button className="text-white bg-blue-400 text-center rounded-lg w-full mt-2 py-3 font-bold hover:bg-blue-500 hover:shadow-lg ">
+                Buy Now
+              </button>
+            </div>
+            <div className="border-2 border-blue-400 rounded-lg p-4 flex flex-col items-center">
+              <div>
+                <img src="images/course.png" alt="course" />
+              </div>
+              <div className="font-bold text-xl pt-3 pb-2">Course Name</div>
+              <div
+                className={`${
+                  readMore ? "" : "line-clamp-2"
+                } text-sm  font-light text-gray-700 tracking-wide text-justify`}
+              >
+                lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem
+                ipsum dolor sit ametlorem ipsum dolor sit amet lorem ipsum dolor
+                sit amet lorem ipsum dolor sit amet
+              </div>
+              <div
+                onClick={readMoreHandler}
+                className="self-end text-xs text-blue-400 font-medium py-2"
+              >
+                {readMore ? "Show Less" : "Read More"}
+              </div>
+              <div className="py-2 flex">
+                <div className="line-through font-bold text-blue-400">
+                  ₹ 400/-
+                </div>
+                <div className="font-bold pl-2">₹ 200/-</div>
+              </div>
+
+              <button className="text-white bg-blue-400 text-center rounded-lg w-full mt-2 py-3 font-bold hover:bg-blue-500 hover:shadow-lg ">
+                Buy Now
+              </button>
+            </div>
+          </div>
+
+          {/* <ProductCard /> */}
+        </div>
+      )}
     </div>
   );
 };
--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -10,32 +10,14 @@ export const NavBar = () => {
   const { totalItemsInCart } = useCountItems();
 
   return (
-    <div className="fixed top-0 z-10 w-full flex flex-row text-2xl text-white items-center justify-around  lg:justify-end bg-blue-400 py-5 shadow">
-      <div className="flex flex-row  w-16  lg:justify-start  lg:w-full lg:px-5 ">
-        <Link to={HOME}>
-          {" "}
-          <Logo nameClass="rounded-full w-12 h-12 lg:h-16 lg:w-16 border shadow-md self-start justify-start" />
-        </Link>
-      </div>
-      <div className="flex flex-row  w-72    justify-around lg:w-full ">
-        {/* <Link to={HOME}> <i className="fas fa-home text-3xl "></i> </Link>
-        <Link to={WISHLIST}>
-          <i className="fas fa-heart text-3xl relative">
-            <span className="absolute left-4 lg:left-7 top-0 rounded-full bg-red-600 w-6 h-6 top right p-0 m-0 text-white font-mono  text-xl leading-tight text-center">
-              {totalItemsInWish}
-            </span>
-          </i>
-        </Link>
-        <Link to={CART}>
-          <i className="fas fa-shopping-cart text-3xl relative">
-            <span className="absolute left-4 lg:left-7 top-0 rounded-full bg-red-600 w-6 h-6 top right p-0 m-0 text-white font-mono  text-xl leading-tight text-center">
-              {totalItemsInCart}
-            </span>
-          </i>
-        </Link>  */}
+    <div className="fixed top-0 z-10 w-full flex  text-2xl text-white items-center justify-between  lg:px-20 bg-blue-400 py-2 shadow px-5">
+      <Link to={HOME}>
+        <Logo nameClass="rounded-full w-12 lg:h-16 lg:w-16 border shadow-md self-start justify-start" />
+      </Link>
+
+      <div>
         {login ? (
-          <Link to={PROFILE}>
-            {" "}
+          <Link to={PROFILE} className="mr-5">
             <i className="fas fa-user-circle text-3xl"></i>
           </Link>
         ) : (
